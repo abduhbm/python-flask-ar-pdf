f00dc5bb7dfa43f59a8a55b0971569b8a0a22211
Abdulelah Bin Mahfoodh <abduh.bm@gmail.com>
2021-05-08 16:13:17 +0300

    Add table

--- api/endpoints/pdf.py
+++ api/endpoints/pdf.py
@@ -7,6 +7,10 @@ from bidi.algorithm import get_display
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib import colors
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.enums import TA_RIGHT
+from reportlab.platypus import Table, TableStyle, Paragraph
 
 from flask import request, send_file
 from flask_restx import Resource
@@ -32,6 +36,18 @@ def write_pdf(input_text):
     canv.setFont('JannaBold', FONT_SIZE)
     canv.drawImage('resources/Python-Logo.png', 0, 0, mask='auto')
     canv.drawRightString(PAGE_SIZE[0] - 10, 10, get_display(arabic_reshaper.reshape(input_text)))
+
+    style = ParagraphStyle(name='Normal', fontName='JannaBold', fontSize=12, leading=12. * 1.2, wordWrap='RTL')
+    style.alignment = TA_RIGHT
+    data = ['نص عربي', input_text]
+    data = [[Paragraph(get_display(arabic_reshaper.reshape(x)), style)] for x in data]
+    f = Table(data, style=TableStyle([
+        ("INNERGRID", (0, 0), (-1, -1), 0.25, colors.black),
+        ("BOX", (0, 0), (-1, -1), 0.25, colors.black)
+    ]))
+    f.wrapOn(canv, 120, 0)
+    f.drawOn(canv, 449, 150)
+
     canv.save()
     packet.seek(0)
     return packet
